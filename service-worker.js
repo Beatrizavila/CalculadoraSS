acb3e22071c95710be0903f6e910c8f7283c6fd6
Beatriz Avila <iheredia@ucol.mx>
2020-12-20 14:53:33 -0600

    hola

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,51 @@
-;
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'v2_cache_programador_fitness',
-  urlsToCache = [
+const cache_name = 'cache2',
+urlsToCache = [
     './',
-    './estilo.css',
+   './estilo.css',
     './calculadora.js',
     './index.html',
     './manifest.webmanifest',
     './img/icons-192.png',
     './img/icons-512.png'
-  ]
+    
+]
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+//para funcionar sin conexion
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
-      .catch(err => console.log('Falló registro de cache', err))
-  )
-})
-
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
-  e.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
+    e.waitUntil(
+        caches.open(cache_name)
+          .then(cache => {
+            return cache.addAll(urlsToCache)
+              .then(() => self.skipWaiting())
           })
-        )
-      })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
-  )
-})
+          .catch(err => console.log('Falló registro de cache', err))
+      )
+    })
+//para funcionar sin conexion
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [cache_name]
 
-//cuando el navegador recupera una url
+    e.waitUntil(
+      caches.keys()
+        .then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              //Eliminamos lo que ya no se necesita en cache
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName)
+              }
+            })
+          )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+    )
+  })
+
+//recuperae archivos
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
+ //Responder ya sea con el objeto en caché o continuar y buscar la url real
+ e.respondWith(
     caches.match(e.request)
       .then(res => {
         if (res) {
